d1960ab2819c7d97ac69f0644b9a9f98a7bb0381
mehtakaran9 <mehtakaran9@gmail.com>
2019-03-11 14:36:21 +0530

    Change SpringBootApplication

--- src/main/java/com/scrapbook/UserProfileMicroservice/UserProfileMicroserviceApplication.java
+++ src/main/java/com/scrapbook/UserProfileMicroservice/UserProfileMicroserviceApplication.java
@@ -1,10 +1,13 @@
 package com.scrapbook.UserProfileMicroservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-public class UserProfileMicroserviceApplication {
+@EnableSwagger2
+public class UserProfileMicroserviceApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserProfileMicroserviceApplication.class, args);
